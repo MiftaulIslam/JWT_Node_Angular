9b307f7275e753425670a54972185b6cb9ddb735
Miftaul_Islam_Ariyan <124516497+MiftaulIslam@users.noreply.github.com>
2024-05-30 00:40:02 +0600

    Update authorizeUser.js

--- backend/controllers/authorization/authorizeUser.js
+++ backend/controllers/authorization/authorizeUser.js
@@ -7,7 +7,7 @@ const AuthorizeUser = async (req, res) => {
         if(!user)return res.status(404).json({error:'User not found', auth:false})
         res.status(200).json({auth:true, message:"User authorized", user:user})
     }catch(err){
-        res.status(500).json({error:"Internal servsdadasdwadadwaasder error", auth: false})
+        res.status(500).json({error:"Internal error occured", auth: false})
     }
 }
 module.exports = {AuthorizeUser}
